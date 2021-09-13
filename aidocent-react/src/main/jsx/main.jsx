7cc6b08c69e478cd50c5ef771a6561f87a4f577c
hssuh <hssuh@DESKTOP-N7V1TO7>
2021-09-13 10:29:30 +0900

    upload 페이지 구현 - 디자인 필요

--- aidocent-react/src/main/jsx/main.jsx
+++ aidocent-react/src/main/jsx/main.jsx
@@ -1,7 +1,29 @@
+import Upload from './upload';
+
 const Main = () => {
+
+    const handleUploadFile = (file) => {
+        let body = new FormData();
+        body.append('file', file);
+        const url = `localhost:8080/aicocent/upload`;
+        let isError = false;
+        fetch(url, {method:"POST", body})
+            .then((response) => {
+                isError = !response.ok;
+                return response.json();
+            }).then((data) => {
+                if(isError)
+                    throw data;
+            }).catch(() => {
+                console.log("에러발생")
+            });
+        };
+
+
     return(
         <div>
             <h1>메인화면 입니다.</h1>
+            <Upload uploadFile={handleUploadFile}/>
         </div>
     );
 };
---
+++ aidocent-react/src/main/jsx/upload.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useDropzone } from 'react-dropzone';
+
+const FileUpload = (props) => {
+
+    const handleDropFile = (acceptedFiles, fileRejections) => {
+        if(fileRejections.length > 0){
+           return;
+        }
+
+        if(acceptedFiles.length > 0){
+            let uploadFiles = [];
+            for(let i in acceptedFiles) {
+                let file = acceptedFiles[i];
+                uploadFiles.push(file);
+            }
+
+            if(uploadFiles.length > 0){
+                for(let i in uploadFiles){
+                    props.uploadFile(uploadFiles[i]);
+                }
+            }
+        }
+    };
+
+    /* fileupload config */
+    const { getRootProps, getInputProps, open } = useDropzone({
+        noClick: true,
+        noKeyboard: true,
+        onDrop: handleDropFile,
+        maxSize: 500000
+    });
+
+    return (
+        <div className="open-file-box clearfix">
+            <div className='open-file' { ...getRootProps() } style={{marginTop:20}}>
+                <input { ...getInputProps() }/>
+                <span className="icon">파일을 여기로 끌어오거나 파일선택 버튼을 눌러주세요.</span>
+                <button className="btn btn-brown tline w100 pos" onClick={ open }>파일 선택...</button>
+            </div>
+        </div>
+    );
+};
+
+export default FileUpload;
